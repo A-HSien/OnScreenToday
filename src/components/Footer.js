a4fd0704d814949bdde363503e0328c1f294fd6f
Robert C Jensen <robert.cole.jensen@gmail.com>
2016-11-15 21:25:56 -0500

    ui: footer responsiveness
    I hid the icon & terms / privacy on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,19 +14,19 @@ class Footer extends Component {
         jsx = (
           <div className="footer container">
             <div className="row">
-              <div className="col-xs-3">
+              <div className="col-sm-3 hidden-xs">
                 <div className="logo"></div>
               </div>
-              <div className="col-xs-3">
+              <div className="col-sm-3 col-xs-6">
                 <MailingListSignup/>
               </div>
-              <div className="link-1 col-sm-2">
+              <div className="link-1 col-sm-2 hidden-xs">
                 <ul>
                     <li className="term">Term of Use</li>
                     <li className="privacy">Privacy Policy</li>
                 </ul>
               </div>
-              <div className="icons col-xs-4">
+              <div className="icons col-sm-4 col-xs-6">
                   <ul>
                       <a target="_blank" href="https://www.facebook.com/pages/Screen-%E4%BB%8B%E9%9D%A2/696130310507423" ><li className="icon fb"></li></a>
                       <a target="_blank" href="https://twitter.com/OnScreenToday?lang=en&hc_location=ufi" ><li className="icon twitter"></li></a>
